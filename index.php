225452ed79625a1d3606e4788d777de24554072c
Illya King <illyaking1978@gmail.com>
2020-05-24 17:04:41 -0700

    Upload
    This is the upload of the final version of the Burrito Masala website.

---
+++ index.php
@@ -0,0 +1,201 @@
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Burrito Masala : Downtown Portland Food Cart serving southern Indian and Mexican Foods">
+    <meta name="generator" content="Jekyll v3.8.6">
+    <title>Burrito Masala : Downtown Portland Food Cart serving southern Indian and Mexican Foods</title>
+    <link href="https://fonts.googleapis.com/css2?family=Lalezar&display=swap" rel="stylesheet">
+    <link href="css/helper.css" rel="stylesheet" type="text/css">
+    <link href="css/grid.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="fontawesome/css/all.css" rel="stylesheet">
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <script src="js/command.js"></script>
+    <script src="js/myBtn.js"></script>
+</head>
+
+<body data-gr-c-s-loaded="true" cz-shortcut-listen="true">
+
+    <header>
+        <nav class="navbar navbar-expand-lg darkNav fixed-top">
+            <div class="container-fluid">
+                <a onclick="topFunction()" title="Go to top" class="navbar-brand"><i class="fas fa-pepper-hot"></i> Burrito Masala</a><br>
+                <a class="navbar-brand phoneNumber" href="tel:1-503-555-5555">503-555-5555</a>
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav">
+                        <li class="nav-link">
+                            <a class="nav-link" href="#build">Build Your Burrito</a><!-- anchor link to the Build Your Burrito Section-->
+                        </li>
+                        <li class="nav-link">
+                            <a class="nav-link" href="#options">Options</a><!-- Anchor link to the options sections of the site. -->
+                        </li>
+                        <li class="nav-link">
+                            <a class="nav-link" href="#hoursandmap">Hours and Map</a><!-- Link to sections in the site -->
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main role="main">
+
+        <!-- Modify Hero Image Statment Here -->
+        <div class="heroImage container-fluid elementToFadeInAndOut">
+            <h1 class="leader">Serving Southern Indian and Mexican Food... <br><!-- Statement -->
+                Stuffed in a Tortilla from our Food Cart!</h1><!-- Statement -->
+        </div><!-- End of Hero Image -->
+
+        <!--Modify Build your burrito and images here -->
+        <div class="container-fluid one">
+            <a class="anchor" id="build"></a>
+            <h1>Build your Burrito or Bowl</h1>
+            <p>Diners can choose the following options for the burrito or bowl- </p>
+            <div class="row one">
+                <div class="col-md-4">
+                    <img class="rounded" src="images/chicken.jpg">
+                    <p>Step 1: Choose one meat or veggie option</p>
+                </div>
+                <div class="col-md-4">
+                    <img class="rounded" src="images/rice.jpg">
+                    <p>Step 2: Choose one rice option</p>
+                </div>
+                <div class="col-md-4">
+                    <img class="rounded" src="images/chutney.jpg">
+                    <p>Step 3: Add Condiments</p>
+                </div>
+            </div>
+        </div><!-- End of Build your Burrito-->
+
+        <!-- Modify Food Offerings Here -->
+        <a class="anchor" id="options"></a>
+        <div class="container-fluid">
+
+            <!-- Modify Food Options here -->
+            <div class="row ">
+                <div class="col-lg-6 two">
+                    <h2>Options</h2>
+                    <div class="row twoSpacing">
+                        <div class="col-lg-4">
+                            <h2>Meat</h2>
+                            <ul>
+                                <li>Chicken Tikka Masala</li>
+                                <li>Lamb Curry</li>
+                                <li>Pork in salsa verde</li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-4">
+                            <h2>Veggie</h2>
+                            <ul>
+                                <li>Sambar (lentil curry, vegan)</li>
+                                <li>Aloo gobi masala (potato and cauliflower, vegan)</li>
+                                <li>Chile relleno (vegetarian, contains cheese)</li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-4 ">
+                            <h2>Rice</h2>
+                            <ul>
+                                <li>Kerala biryani</li>
+                                <li>Spanish rice</li>
+                                <li>Tomato</li>
+                                <li>White or brown rice (khaima)</li>
+                                <li>* All Options Vegan</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- End Food Offerings here -->
+
+                    <!-- Modify Condiments Offerings here -->
+                    <h2>Condiments</h2>
+                    <div class="row twoSpacing">
+                        <div class="col-lg-4">
+                            <ul>
+                                <li>Mango Chutney</li>
+                                <li>Tomatillo Salsa</li>
+                                <li>Raita</li>
+                                <li>Grandma Nena’s Fiery Red Salsa</li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-4">
+                            <ul>
+                                <li>Cheese</li>
+                                <li>Sour cream</li>
+                                <li>Onions</li>
+                                <li>Tomatoes</li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-4">
+                            <ul>
+                                <li>Cucumbers</li>
+                                <li>Cilantro</li>
+                                <li>Tomato</li>
+                                <li>Guacamole ($.75 extra!)</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Modify Condiments Offerings here -->
+
+                </div>
+                <div class="col-lg-6 twoImage">
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-4">
+                    <a class="anchor" id="hoursandmap"></a>
+                    <!-- Modify Food Cart Description Here -->
+                    <h1>About Burrito Masala</h1>
+                    <p>Burrito Masala is food cart located in downtown Portland, OR that specializes in delicious southern Indian (Kerala) and Mexican foods… stuffed in a flour tortilla! Diners can also opt to go gluten free with our bowl options. We’re run by a husband and wife team (James Beard award-winning chefs Prasad Varma and Ramona Varma-Hernandez).</p>
+                    <!-- Ending Food Cart Description Here -->
+
+                    <!-- Modify Social Media Links Here -->
+                    <h1>Follow Burrito Masala</h1>
+                    <ul>
+                        <li><a href="https://facebook.com" target="_blank"><i class="fab fa-facebook-square"></i></a></li>
+                        <li><a href="https://instagram.com" target="_blank"><i class="fab fa-instagram-square"></i></a></li>
+                        <li><a href="https://pinterest.com" target="_blank"><i class="fab fa-pinterest-square"></i></a></li>
+                        <li><a href="https://twitter.com" target="_blank"><i class="fab fa-twitter-square"></i></a></li>
+                    </ul>
+                </div>
+                <!-- End Social Media Links Here -->
+
+                <!-- Modify Hours and Contact Info Here -->
+                <div class="col-lg-3">
+                    <h1>Our Hours</h1>
+                    <p>Open 7 days a week<br>
+                        11:00 AM to 7:00 PM</p>
+                    <br>
+                    <h1>Contact Us</h1>
+                    <p><i class="fas fa-map-marker-alt"></i> SW 3rd Ave & SW Alder St<p>
+                            <p class="spacerBoi"> Portland, OR 97204</p>
+                            <p><i class="fas fa-mobile-alt"></i> <a href="tel:1-503-555-5555">503-555-5555</a></p>
+                </div>
+                <!-- End Hours and Contact Info Here -->
+
+                <!-- Modify Google Map -->
+                <div class="col-lg-5">
+                    <h1>Where we are located</h1>
+                    <div class="google-maps">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2795.560610598459!2d-122.67735768423182!3d45.51892303781085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54950a05e9f31c37%3A0x147e3ce29232d353!2sSW%203rd%20Ave%20%26%20SW%20Alder%20St%2C%20Portland%2C%20OR%2097204!5e0!3m2!1sen!2sus!4v1589737317260!5m2!1sen!2sus" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    </div>
+                </div>
+                <!-- End Google Map -->
+
+            </div>
+        </div>
+        <div class="container-fluid copyright">
+            <p class="copyright">@ <?php echo date('Y'); ?> Burrtio Masala. All Rights Reserved.</p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
